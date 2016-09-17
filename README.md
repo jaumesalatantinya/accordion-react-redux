33f0ac9500c8f307963994be3481cdac5efd40b1
Jaume Sala Tantinya <jaumesalatantinya@gmail.com>
2016-09-17 22:17:45 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-## React Redux Workshop Example 4 App
+## Accordion made using React and Redux
+
+<li>npm install</li>
+<li>npm run start -s</li>
+<li>http://localhost:3000/</li>
+<li>npm run build</li>
+<li>npm run dev</li>
+<li>npm run test</li>
+<li>npm run lint</li>
--- tools/buildHtml.js
+++ tools/buildHtml.js
@@ -1,6 +1,3 @@
-// This script copies src/index.html into /dist/index.html
-// This is a good example of using Node and cheerio to do a simple file transformation.
-// In this case, the transformation is useful since we only use a separate css file in prod.
 import fs from 'fs';
 import cheerio from 'cheerio';
 import colors from 'colors';
@@ -15,7 +12,7 @@ fs.readFile('src/index.html', 'utf8', (err, markup) => {
   const $ = cheerio.load(markup);
 
   // since a separate spreadsheet is only utilized for the production build, need to dynamically add this here.
-  $('head').prepend('<link rel="stylesheet" href="styles.scss">');
+  $('head').prepend('\n\t\t<link rel="stylesheet" href="styles.css">');
 
   fs.writeFile('dist/index.html', $.html(), 'utf8', function (err) {
     if (err) {
