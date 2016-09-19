0457535ae2b67e5d9d8947f81efc4716ed662cb7
Jaume Sala Tantinya <jaume.salas@privalia.com>
2016-09-19 11:21:01 +0200

    Move from accordion to sections

--- src/reducers/sectionsReducers.js
+++ src/reducers/sectionsReducers.js
@@ -1,7 +1,7 @@
 import * as types from '../actions/actionTypes';
 import initialState from './initialState';
 
-export default function accordionReducers (state = initialState.sections, action) {
+export default function sectionsReducers (state = initialState.sections, action) {
 
     switch (action.type) {
         case types.GET_SECTIONS_SUCCESS:
